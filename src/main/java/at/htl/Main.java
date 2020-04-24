320a8ad523e0ee9d72738095900de55265ba2860
Lukas Starka <lstarka141000@gmail.com>
2020-04-24 10:23:47 +0200

    added another thread and joined them both at the end

--- src/main/java/at/htl/Main.java
+++ src/main/java/at/htl/Main.java
@@ -1,21 +1,39 @@
 package at.htl;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Counter.getInstance().initialize(500);
 
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 40; i++) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println("Thread 1 increments counter");
+                Counter.getInstance().increment(10);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Thread 2 increments counter");
                 Counter.getInstance().increment(10);
             }
         });
         
         thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
         System.out.println("Main finished");
     }
 }
