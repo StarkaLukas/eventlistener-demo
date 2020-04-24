ed2048b3cf0f6b5a5a10ea398749b9bfe1220708
Lukas Starka <lstarka141000@gmail.com>
2020-04-24 10:18:35 +0200

    used counter with one thread

--- src/main/java/at/htl/Main.java
+++ src/main/java/at/htl/Main.java
@@ -2,6 +2,20 @@ package at.htl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Counter.getInstance().initialize(500);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 50; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Counter.getInstance().increment(10);
+            }
+        });
+        
+        thread1.start();
+        System.out.println("Main finished");
     }
 }
