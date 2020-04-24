747f6132a7293d411fd55b74903a69f5c3f794a2
Lukas Starka <lstarka141000@gmail.com>
2020-04-24 10:52:47 +0200

    deleted every logging message except for when counter was incremented

--- src/main/java/at/htl/Main.java
+++ src/main/java/at/htl/Main.java
@@ -3,36 +3,34 @@ package at.htl;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Counter.getInstance().initialize(500);
+        Counter.getInstance().initialize(520);
 
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 40; i++) {
+            for (int i = 0; i < 50; i++) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Thread 1 increments counter");
                 Counter.getInstance().increment(10);
             }
         });
 
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 20; i++) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Thread 2 increments counter");
                 Counter.getInstance().increment(10);
             }
         });
 
-        Counter.getInstance().addListener(propertyChangeEvent -> {
-            System.out.println("Got info that limit was reached");
+        Counter.getInstance().addListener(event -> {
+            System.out.println("Counter was incremented from " + event.getOldValue() + " to " + event.getNewValue());
         });
-        
+
         thread1.start();
         thread2.start();
 
